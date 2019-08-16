2bb4494f1d2543a339db57302fb42e1d7a8c991d
sanduni <sandunipavithra9611@gmail.com>
2019-08-16 14:49:18 +0530

    issue#8:User Registration flow

--- routes/users.js
+++ routes/users.js
@@ -2,36 +2,55 @@ var express = require('express');
 var router = express.Router();
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt');
+const { check, validationResult } = require('express-validator');
 
 const Users = require('../models/users');
 
 router.use(bodyParser.json());
 
-router.post('/', function(req, res, next) {
-  let user = req.body;
+router.post('/', [
+  // name must be an email
+  check('email').isEmail(),
+  // password must be at least 5 chars long
+  check('password').isLength({ min: 5 })
+],function(req, res, next) {
+  const errors = validationResult(req);
 
-  new Promise((resolve, reject) => {
-    bcrypt.hash(user.password, 10, (err, hash) => {
-      if (err) {
-        reject(err);
-      } else {
-        user.password = hash;
-        resolve(user);
-      }
-    });
-  })
-    .then(user => {
-      return Users.create(user);
-    })
-    .then(user => {
-      console.log('user created', user.name, user.email);
-      res.statusCode = 200;
-      res.setHeader('Content-Type', 'application/json');
-      res.json({ id: user._id, name: user.name, email: user.email });
-    })
-    .catch(err => {
-      next(err);
-    });
+  if (!errors.isEmpty()) {
+    res.status(422).json({ errors: errors.array() });
+  } else {
+
+    let user = req.body;
+
+    Users.findOne({ email: user.email })
+      .then(found => {
+        if (found) {
+          const error = new Error('please use another email');
+          error.status = 403;
+          throw error;
+        } else {
+          return new Promise((resolve, reject) => {
+            bcrypt.hash(user.password, 10, (err, hash) => {
+              if (err) {
+                reject(err);
+              } else {
+                user.password = hash;
+                resolve(user);
+              }
+            });
+          });
+        }
+      })
+      .then(user => {
+        return Users.create(user);
+      })
+      .then(user => {
+        res.status(200).json({ id: user._id, name: user.name, email: user.email });
+      })
+      .catch((err) => {
+        next(err);
+      });
+  }
 });
 
 module.exports = router;
