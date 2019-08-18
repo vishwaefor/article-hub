4b26c9c8af0fc07b21da832b8a5d21e2e7393581
dilini <dilineetharaka.com>
2019-08-18 22:18:13 +0530

    #7:Register user

--- routes/users.js
+++ routes/users.js
@@ -6,5 +6,38 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt');
+
+const Users = require('../models/users');
+
+router.use(bodyParser.json());
+
+router.post('/', function(req, res, next) {
+  let user = req.body;
+
+  new Promise((resolve, reject) => {
+    bcrypt.hash(user.password, 10, (err, hash) => {
+      if (err) {
+        reject(err);
+      } else {
+        user.password = hash;
+        resolve(user);
+      }
+    });
+  })
+    .then(user => {
+      return Users.create(user);
+    })
+    .then(user => {
+      console.log('user created', user.name, user.email);
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'application/json');
+      res.json({ id: user._id, name: user.name, email: user.email });
+    })
+    .catch(err => {
+      next(err);
+    });
+});
 
 module.exports = router;
