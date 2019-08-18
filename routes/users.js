5edcd82459a6e1107c8e764a36d9ae23e6c60052
Pasindu <piyathilaka10@gmail.com>
2019-08-18 16:34:37 +0530

    issue-6:Create User routes

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+// router.get('/', function(req, res, next) {
+//   res.send('respond with a resource');
+// });
+
+// module.exports = router;
+
+
+const express = require('express');
+const router = express.Router();
 
 module.exports = router;
