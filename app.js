a7ae5b84043825ef70602b604c931bad1112ce42
tharuash <tharinduash@outlook.com>
2019-08-19 03:16:05 +0530

    #10 Create routes for adding an article

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+var articlesRouter = require('./routes/articles');
 
 
 var app = express();
@@ -36,21 +37,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/articles', articlesRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
-});
-
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
 });
 
 // error handler
--- routes/users.js
+++ routes/users.js
@@ -1,56 +1,16 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const bodyParser = require('body-parser');
-const bcrypt = require('bcrypt');
+const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt");
 const {
   check,
   validationResult
-} = require('express-validator');
+} = require("express-validator");
 
-const Users = require('../models/users');
+const Users = require("../models/users");
 
 router.use(bodyParser.json());
 
-
-
-/* GET users listing. */
-router.get('/', function (req, res, next) {
-  res.send('respond with a resource');
-});
-
-/*POST */
-router.post('/', function (req, res, next) {
-  let user = req.body;
-
-  new Promise((resolve, reject) => {
-      bcrypt.hash(user.password, 10, (err, hash) => {
-        if (err) {
-          reject(err);
-        } else {
-          user.password = hash;
-          resolve(user);
-        }
-      });
-    })
-    .then(user => {
-      return Users.create(user);
-    })
-    .then(user => {
-      console.log('user created', user.name, user.email);
-      res.statusCode = 200;
-      res.setHeader('Content-Type', 'application/json');
-      res.json({
-        id: user._id,
-        name: user.name,
-        email: user.email
-      });
-    })
-    .catch(err => {
-      next(err);
-    });
-});
-
-/* POST validate*/
 router.post('/', [
   // name must be an email
   check('email').isEmail(),
@@ -68,48 +28,44 @@ router.post('/', [
     });
   } else {
 
-    //rest of the code
+    let user = req.body;
+
+    Users.findOne({
+        email: user.email
+      })
+      .then(found => {
+        if (found) {
+          const error = new Error('please use another email');
+          error.status = 403;
+          throw error;
+        } else {
+          return new Promise((resolve, reject) => {
+            bcrypt.hash(user.password, 10, (err, hash) => {
+              if (err) {
+                reject(err);
+              } else {
+                user.password = hash;
+                resolve(user);
+              }
+            });
+          });
+        }
+      })
+      .then(user => {
+        return Users.create(user);
+      })
+      .then(user => {
+        res.status(200).json({
+          id: user._id,
+          name: user.name,
+          email: user.email
+        });
+      })
+      .catch((err) => {
+        next(err);
+      });
 
   }
 });
-
-
-let user = req.body;
-
-Users.findOne({
-    email: user.email
-  })
-  .then(found => {
-    if (found) {
-      const error = new Error('please use another email');
-      error.status = 403;
-      throw error;
-    } else {
-      return new Promise((resolve, reject) => {
-        bcrypt.hash(user.password, 10, (err, hash) => {
-          if (err) {
-            reject(err);
-          } else {
-            user.password = hash;
-            resolve(user);
-          }
-        });
-      });
-    }
-  })
-  .then(user => {
-    return Users.create(user);
-  })
-  .then(user => {
-    res.status(200).json({
-      id: user._id,
-      name: user.name,
-      email: user.email
-    });
-  })
-  .catch((err) => {
-    next(err);
-  });
-
 
 module.exports = router;
