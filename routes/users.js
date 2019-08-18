306a2242be4f7addf0896fb56a705869f8de5080
Kamith Yudara <54225931+KamithYudara@users.noreply.github.com>
2019-08-18 20:28:10 +0530

    #9: Update error handler to give an JSON always

--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,7 @@ var router = express.Router();
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt');
 
+const { check, validationResult } = require('express-validator');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -41,5 +42,61 @@ router.post('/', function(req, res, next) {
       next(err);
     });
 });
+
+
+/*Validate request and if the body contains a valid email and a password*/
+
+router.post('/', [
+  // name must be an email
+  check('email').isEmail(),
+  // password must be at least 5 chars long
+  check('password').isLength({ min: 5 })
+], (req, res, next) => {
+
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    res.status(422).json({ errors: errors.array() });
+  } else {
+
+    //rest of the code
+
+  }
+});
+
+
+
+/*Update rest of the code to check the database for existing email and inform the user to use another email*/
+
+let user = req.body;
+
+ Users.findOne({ email: user.email })
+   .then(found => {
+     if (found) {
+       const error = new Error('please use another email');
+       error.status = 403;
+       throw error;
+     } else {
+       return new Promise((resolve, reject) => {
+         bcrypt.hash(user.password, 10, (err, hash) => {
+           if (err) {
+             reject(err);
+           } else {
+             user.password = hash;
+             resolve(user);
+           }
+         });
+       });
+     }
+   })
+   .then(user => {
+     return Users.create(user);
+   })
+   .then(user => {
+     res.status(200).json({ id: user._id, name: user.name, email: user.email });
+   })
+   .catch((err) => {
+     next(err);
+   });
 
 module.exports = router;
