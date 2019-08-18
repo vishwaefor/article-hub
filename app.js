80e53c8224a17da9d7469f50678205d25c1cad58
Pasindu <piyathilaka10@gmail.com>
2019-08-18 16:55:28 +0530

    issue-9:Update error handler to give an JSON always

--- app.js
+++ app.js
@@ -50,3 +50,13 @@ connection .then((db) => { console.log("Connected correctly to server"); }) .cat
 
 
 
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // JSON error
+  res.status(err.status || 500);
+  res.json({ errors: [{ msg: err.message }] }); // change render to json method
+});
