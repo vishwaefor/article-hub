d595cab5eae69a81fa26bdb0812df4f86c9ae7a0
Gayan <gayandilankak@gmail.com>
2019-08-19 01:33:08 +0530

    #7 Register user

--- routes/users.js
+++ routes/users.js
@@ -9,6 +9,45 @@ router.get('/', function(req, res, next) {
 const express = require('express');
 const router = express.Router();
 
+const bodyParser = require('body-parser');
+router.use(bodyParser.json());
 
+
+
+var express = require('express');
+var router = express.Router();
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt');
+
+const Users = require('../models/users');
+
+router.use(bodyParser.json());
+
+router.post('/', function(req, res, next) {
+  let user = req.body;
+
+  new Promise((resolve, reject) => {
+    bcrypt.hash(user.password, 10, (err, hash) => {
+      if (err) {
+        reject(err);
+      } else {
+        user.password = hash;
+        resolve(user);
+      }
+    });
+  })
+    .then(user => {
+      return Users.create(user);
+    })
+    .then(user => {
+      console.log('user created', user.name, user.email);
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'application/json');
+      res.json({ id: user._id, name: user.name, email: user.email });
+    })
+    .catch(err => {
+      next(err);
+    });
+});
 
 module.exports = router;
