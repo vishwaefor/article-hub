b85835b9e76989e8c4f111bfc905a4999290f057
Chamara Madhushan <cmb.info96@gmail.com>
2019-08-20 00:30:53 +0530

    #10 Create routes for adding an article

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+var articlesRouter = require('./routes/articles');
 
 var app = express();
 
@@ -26,6 +27,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+app.use('/articles', articlesRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
