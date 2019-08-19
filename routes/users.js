3d8623db2e5b56751c0bcae24543e84d315f680d
asitha <yaasithasamarasekara@gmail.com>
2019-08-20 00:15:55 +0530

    #6 Create User routes

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const bodyParser = require('body-parser');
-router.use(bodyParser.json());
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
