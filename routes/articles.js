9d5df15c1f889955e2caeb36a0a63722525f956a
AnushangaWimalasena <anushangawimalasena95@gmail.com>
2019-08-19 22:23:56 +0530

    #12:Create endpoints for retrieving all the articles and single article giving the id

--- routes/articles.js
+++ routes/articles.js
@@ -54,4 +54,53 @@ router.post(
   }
 );
 
+router.get('/', (req, res, next) => {
+    Articles.find()
+      .populate('author')
+      .then(results => {
+        res.status(200).json({
+          results: results.map(r => {
+            return {
+              id: r.id,
+              title: r.title,
+              content: r.content,
+              image: r.image,
+              author:
+                (r.author && { id: r.author._id, name: r.author.name }) || null
+            };
+          })
+        });
+      })
+      .catch(err => next(err));
+  });
+
+  router.get('/:id', (req, res, next) => {
+    Articles.findById(req.params.id)
+      .populate('author', 'comments.author')
+      .then(
+        r => {
+          if (r) {
+            res.status(200).json({
+              id: r.id,
+              title: r.title,
+              content: r.content,
+              image: r.image,
+              author:
+                (r.author && { id: r.author._id, name: r.author.name }) || null
+            });
+          } else {
+            const error = new Error('no article found');
+            error.status = 404;
+            throw error;
+          }
+        },
+        err => {
+          const error = new Error('invalid article id');
+          error.status = 400;
+          throw error;
+        }
+      )
+      .catch(err => next(err));
+  });
+
 module.exports = router;
