3a0df81f827a0170fb12d0ddd304f40a08e50aef
ashikahansini <ashihansini944@gmail.com>
2019-08-20 13:37:24 +0530

    #8 Add validations to user registration flow

--- routes/users.js
+++ routes/users.js
@@ -5,6 +5,8 @@ const router = express.Router();
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt');
 
+const { check, validationResult } = require('express-validator');
+
 const Users = require('../models/users');
 
 router.use(bodyParser.json());
@@ -36,6 +38,52 @@ router.post('/', function(req, res, next) {
     });
 });
 
+router.post('/', [
+  // name must be an email
+  check('email').isEmail(),
+  // password must be at least 5 chars long
+  check('password').isLength({ min: 5 })
+], (req, res, next) => {
+
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    res.status(422).json({ errors: errors.array() });
+  } else {
+
+    let user = req.body;
+
+ Users.findOne({ email: user.email })
+   .then(found => {
+     if (found) {
+       const error = new Error('please use another email');
+       error.status = 403;
+       throw error;
+     } else {
+       return new Promise((resolve, reject) => {
+         bcrypt.hash(user.password, 10, (err, hash) => {
+           if (err) {
+             reject(err);
+           } else {
+             user.password = hash;
+             resolve(user);
+           }
+         });
+       });
+     }
+   })
+   .then(user => {
+     return Users.create(user);
+   })
+   .then(user => {
+     res.status(200).json({ id: user._id, name: user.name, email: user.email });
+   })
+   .catch((err) => {
+     next(err);
+   });
+
+  }
+});
 
 
 /* GET users listing. */
