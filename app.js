8166fae8489b3ec36a28375596a018997638573c
Udeshi1 <udeshithilakarathne10@gmail.com>
2019-08-21 01:28:57 +0530

    #2 Introduce MongoDB with Mongoose

--- app.js
+++ app.js
@@ -15,7 +15,9 @@ app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -23,12 +25,12 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -36,6 +38,17 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+
+
+const mongoose = require('mongoose');
+const connection = mongoose.connect('mongodb+srv://udeshi:udeshi1234@testcluster-lltrf.mongodb.net/test?retryWrites=true&w=majority', {
+  useNewUrlParser: true
+});
+connection.then((db) => {
+  console.log("Connected correctly to server");
+}).catch((err) => {
+  console.log(err)
 });
 
 module.exports = app;
