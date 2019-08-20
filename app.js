183c88bbef1711ed188bef7af0463d366f048c4c
Piyumi <piyumimalkisara918@gmail.com>
2019-08-20 13:56:23 +0530

    #2:Introduce mongoDB with Mongoose

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const mongoose = require('mongoose');
+const connection = mongoose.connect('mongodb+srv://piyumiuor:qwer@cluster0-rvpk0.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser: true});
+connection .then((db) => { console.log("Connected correctly to server"); }) .catch((err) => { console.log(err) });
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
