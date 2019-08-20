5ef875fe872033438f1764633e091385b2287c35
kanishka95-cloud <kanishkanishshaka@gmail.com>
2019-08-20 13:57:53 +0530

    #14 implement endpoints for other action

--- routes/articles.js
+++ routes/articles.js
@@ -149,3 +149,201 @@ router.post(
         .catch(err => next(err));
     }
   );
+  router.get('/:id/comments', (req, res, next) => {
+    Articles.findById(req.params.id)
+      .then(
+        r => {
+          if (r) {
+            return Comments.find({
+              _id: {
+                $in: r.comments
+              }
+            }).populate('author');
+          } else {
+            const error = new Error('no article found');
+            error.status = 404;
+            throw error;
+          }
+        },
+        err => {
+          const error = new Error('invalid article id');
+          error.status = 400;
+          throw error;
+        }
+      )
+  
+      .then(comments => {
+        res.status(200).json({
+          results: comments.map(c => {
+            return {
+              id: c._id,
+              comment: c.comment,
+              author: { name: c.author.name, id: c.author._id },
+              createdAt: c.createdAt
+            };
+          })
+        });
+      })
+      .catch(err => next(err));
+  });
+
+  //POST / articles /: id / comments should be implemented like in above issues
+router.post(
+  '/:id/comments',
+  [
+      // title should be given
+      check('comment')
+      .exists()
+      .withMessage('comment is not provided')
+      .isLength({
+          min: 10
+      })
+      .withMessage('minimum 10 charactors required for comment')
+  ],
+  (req, res, next) => {
+      Articles.findById(req.params.id)
+          .populate('author', 'comments.author')
+          .then(
+              r => {
+                  if (r) {
+                      const errors = validationResult(req);
+
+                      if (!errors.isEmpty()) {
+                          res.status(422).json({
+                              errors: errors.array()
+                          });
+                      } else {
+                          const comment = req.body;
+                          Users.findOne() // We are tempory adding a user as the author
+                              .then(author => {
+                                  comment.author = author._id;
+                                  Comments.create(comment)
+                                      .then(comment => {
+                                          r.comments.push(comment);
+                                          return r.save();
+                                      })
+                                      .then(r => {
+                                          res.status(201).json({
+                                              message: 'comment added'
+                                          });
+                                      });
+                              });
+                      }
+                  } else {
+                      const error = new Error('no article found');
+                      error.status = 404;
+                      throw error;
+                  }
+              },
+              err => {
+                  const error = new Error('invalid article id');
+                  error.status = 400;
+                  throw error;
+              }
+          )
+
+          .catch(err => next(err));
+  }
+);
+
+//GET /articles/:id/comments for retrieving all the comments for an article
+router.get('/:id/comments', (req, res, next) => {
+  Articles.findById(req.params.id)
+      .then(
+          r => {
+              if (r) {
+                  return Comments.find({
+                      _id: {
+                          $in: r.comments
+                      }
+                  }).populate('author');
+              } else {
+                  const error = new Error('no article found');
+                  error.status = 404;
+                  throw error;
+              }
+          },
+          err => {
+              const error = new Error('invalid article id');
+              error.status = 400;
+              throw error;
+          }
+      )
+
+      .then(comments => {
+          res.status(200).json({
+              results: comments.map(c => {
+                  return {
+                      id: c._id,
+                      comment: c.comment,
+                      author: {
+                          name: c.author.name,
+                          id: c.author._id
+                      },
+                      createdAt: c.createdAt
+                  };
+              })
+          });
+      })
+      .catch(err => next(err));
+});
+
+//PUT /articles/:id for editing an article
+router.put('/:id', (req, res, next) => {
+  Articles.findByIdAndUpdate(req.params.id, {
+          $set: req.body
+      })
+      .populate('author', 'comments.author')
+      .then(
+          r => {
+              if (r) {
+                  res.status(200).json({
+                      id: r.id,
+                      title: r.title,
+                      content: r.content,
+                      image: r.image,
+                      author: (r.author && {
+                          id: r.author._id,
+                          name: r.author.name
+                      }) || null
+                  });
+              } else {
+                  const error = new Error('no article found');
+                  error.status = 404;
+                  throw error;
+              }
+          },
+          err => {
+              const error = new Error('invalid article id');
+              error.status = 400;
+              throw error;
+          }
+      )
+      .catch(err => next(err));
+});
+
+//DELETE /articles/:id for deleting an article
+router.delete('/:id', (req, res, next) => {
+  Articles.findByIdAndDelete(req.params.id)
+      .then(
+          r => {
+              if (r) {
+                  res.status(200).json({
+                      id: r.id,
+                      status: "Article " + r.id + " has removed."
+                  });
+              } else {
+                  const error = new Error('no article found');
+                  error.status = 404;
+                  throw error;
+              }
+          },
+          err => {
+              const error = new Error('invalid article id');
+              error.status = 400;
+              throw error;
+          }
+      )
+      .catch(err => next(err));
+});
+module.exports = router;
