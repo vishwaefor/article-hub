05338b635071f016ed246f783c8258dac0491757
nimmiupeksha <madhumalipvnu@gmail.com>
2019-08-20 13:50:42 +0530

    #10 Create routes for adding an article

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ connection .then((db) => { console.log("Connected correctly to server"); }) .cat
 
 var indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+var articlesRouter = require('./routes/articles');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/articles', articlesRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
