8b1164ce9e035c1c4178e9b212f7bd8577994189
samu03 <samurdhikanchana12@gmail.com>
2019-08-20 23:06:12 +0530

    #9 Update error handler to give an JSON always

--- app.js
+++ app.js
@@ -34,11 +34,15 @@ app.use(function (req, res, next) {
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+  // JSON error
   res.status(err.status || 500);
-  res.render("error");
+  res.json({
+    errors: [{
+      msg: err.message
+    }]
+  }); // change render to json method
 });
 
 const mongoose = require('mongoose');
