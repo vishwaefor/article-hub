3c155d894e4d84865f16ce4fa80747fbb78b41e5
sachithBG <sachithguruge@gmail.com>
2019-08-21 00:15:22 +0530

    #8:Add validations to user registration flow

--- routes/users.js
+++ routes/users.js
@@ -4,11 +4,55 @@ var router = express.Router();
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt');
 const Users = require('../models/users');
+const { check, validationResult } = require('express-validator');
 router.use(bodyParser.json());
 
 
-router.post('/', function(req, res, next) {
+router.post('/', [
+  // name must be an email
+  check('email').isEmail(),
+  // password must be at least 5 chars long
+  check('password').isLength({ min: 5 })
+], (req, res, next) =>{
+
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    res.status(422).json({ errors: errors.array() });
+  } else {
+
+
   let user = req.body;
+  
+  Users.findOne({ email: user.email })
+  .then(found => {
+    if (found) {
+      const error = new Error('please use another email');
+      error.status = 403;
+      throw error;
+    } else {
+      return new Promise((resolve, reject) => {
+        bcrypt.hash(user.password, 10, (err, hash) => {
+          if (err) {
+            reject(err);
+          } else {
+            user.password = hash;
+            resolve(user);
+          }
+        });
+      });
+    }
+  })
+  .then(user => {
+    return Users.create(user);
+  })
+  .then(user => {
+    res.status(200).json({ id: user._id, name: user.name, email: user.email });
+  })
+  .catch((err) => {
+    next(err);
+  });
+  
 
   new Promise((resolve, reject) => {
     bcrypt.hash(user.password, 10, (err, hash) => {
@@ -32,6 +76,7 @@ router.post('/', function(req, res, next) {
     .catch(err => {
       next(err);
     });
+  }
 });
 
 /* GET users listing. */
