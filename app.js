afd2657171fe9b4fe1c9f88fa41816eb87d412b2
samu03 <samurdhikanchana12@gmail.com>
2019-08-20 22:56:28 +0530

    #6 Create User routes

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
 var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
+const usersRouter = require('./routes/users');
+app.use('/users', usersRouter);
 
 var app = express();
 
@@ -22,7 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
+
 
 // catch 404
 app.use(function (req, res, next) {
