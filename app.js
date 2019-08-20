7e91ed5ac8d6702cdb7a1b215501253247acf6f6
ashikahansini <ashihansini944@gmail.com>
2019-08-20 13:43:48 +0530

    #9 Update error handler to give an JSON always

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.json({ errors: [{ msg: err.message }] }); // change render to json method
 });
 
 module.exports = app;
